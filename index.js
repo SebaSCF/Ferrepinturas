a358f9405c55f523e36d95a9628273fefdeaff74
SebaSCF <49295327+SebaSCF@users.noreply.github.com>
2020-12-04 13:07:09 -0600

    Added the whole project

---
+++ index.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+  sr.reveal('.Dedica', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.Somos', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.Header', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.Contacto', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });
